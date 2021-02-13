c80bca81818ca565e610b0eeca0aa1b2b11b0469
kingendzone <vitalashigarev@outlook.com>
2021-02-13 18:04:51 +0800

    Create me.py

---
+++ me.py
@@ -0,0 +1,18 @@
+
+class GDManjaro:
+    age = None
+    name = 'Vitaliy'
+    language='RUS'
+    friends = ['mezhenov', 'irslan4ik', 'Golemmik']
+    favourite = {
+        'food' : 'pizza',
+        'programming language' : ['Python', 'Javascript','HTML/CSS'],
+        'games' : ['Roblox', 'Minecraft'],
+        'website' : 'www.youtube.com',
+        'color' : 'orange'
+    }
+
+    inventory = {
+        'phone' : 'Samsung A31',
+        'computer' : 'lenovo'
+    }
